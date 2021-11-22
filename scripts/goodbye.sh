a26d37b6daeea24ccd8adac2768df36770d6cc76
Francesco Pezzato <fpezzato@apple.com>
2021-11-22 14:08:18 +0000

    WIP58 - detect PR

--- scripts/goodbye.sh
+++ scripts/goodbye.sh
@@ -1,1 +1,20 @@
-echo "I am an echo in a script"
+#!/bin/bash
+#
+# Deploy a jar, source jar, and javadoc jar to Sonatype's snapshot repo.
+#
+
+SLUG="shazam/fork"
+BRANCH="main"
+
+set -e
+
+#if [ "$GITHUB_REPOSITORY" != "$SLUG" ]; then
+#  echo "Skipping snapshot deployment: wrong repository. Expected '$SLUG' but was '$GITHUB_REPOSITORY'."
+#elif [ "$GITHUB_IS_PULL_REQUEST" != "false" ]; then
+#  echo "Skipping snapshot deployment: was pull request."
+#elif [ "$GITHUB_REF_NAME" != "$BRANCH" ]; then
+#  echo "Skipping snapshot deployment: wrong branch. Expected '$BRANCH' but was '$GITHUB_REF_NAME'."
+#fi
+if [ "$GITHUB_IS_PULL_REQUEST" != "false" ]; then
+  echo "Skipping snapshot deployment: wrong repository. Expected '$SLUG' but was '$GITHUB_REPOSITORY'."
+fi
