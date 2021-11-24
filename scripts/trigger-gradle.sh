0ca98f40aa1d947a8d11d0376f004cada636da38
Francesco Pezzato <fpezzato@apple.com>
2021-11-24 11:52:58 +0000

    WIP79 - secrets in gradle

--- scripts/trigger-gradle.sh
+++ scripts/trigger-gradle.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "From the .sh: $SECRET_2_KEY"
-./gradlew printVariables -DMY_KEY=MyValue -DSECRET_2_KEY=$SECRET_2_KEY
+./gradlew -DMY_KEY=MyValue -DSECRET_2_KEY=$SECRET_2_KEY printVariables
